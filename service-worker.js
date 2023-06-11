4126dbd948e26c23ea3aa1aad09238984175ecea
Otto Mättas <ottomattas@gmail.com>
2023-06-11 19:30:59 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+// Service Worker installation event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('your-app-cache').then((cache) => {
+        // Cache the core assets of your app
+        return cache.addAll([
+          'index.html',
+          'app.js',
+          'styles.css',
+          // Add other core assets here
+        ]);
+      })
+    );
+  });
+  
+  // Service Worker activation event
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        // Remove outdated caches
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== 'your-app-cache') {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+  
+  // Service Worker fetch event
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        // Return cached response if available, otherwise fetch from the network
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
